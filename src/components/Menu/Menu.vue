0a71b338ab785201ecd7aa9ee10acd3f3c545355
phiquecr <hiquicr.ofc@gmail.com>
2022-11-02 11:36:26 -0300

    feat: create header and menu

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <a href="#">{{ links[0] }}</a>
+      </li>
+      <li>
+        <a href="#">{{ links[1] }}</a>
+      </li>
+      <li>
+        <a href="#">{{ links[2] }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MenuItem",
+  data() {
+    return {
+      links: ["Home", "Portfolio", "Contact"],
+    };
+  },
+});
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  margin: 0 6rem;
+}
+ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4rem;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: aliceblue;
+}
+a:hover {
+  color: blueviolet;
+  transition: 0.3s;
+}
+</style>
