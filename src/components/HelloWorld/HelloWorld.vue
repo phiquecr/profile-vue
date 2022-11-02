7d13550c6b84e5d43e1049011d61ec3168de09b1
phiquecr <hiquicr.ofc@gmail.com>
2022-11-02 11:07:04 -0300

    fix: configuring branch develop

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  data() {
+    return {
+      title: "HelloWorld",
+      message: "This commit signals the start of the project.",
+    };
+  },
+});
+</script>
+
+<style scoped>
+.hello {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 0.5rem;
+  font-family: sans-serif;
+}
+.hello h1 {
+  font-size: 2rem;
+}
+.hello p {
+  font-size: 1rem;
+}
+</style>
